a2edbb5c3900e79b2f5d658d009ea06b09eeb62d
juanarango86 <juanarango86@gmail.com>
2023-07-24 15:42:45 -0500

    creacion del proyecto
    creacion del proyecto

---
+++ JuanArangoXamarinApp/JuanArangoXamarinApp/JuanArangoXamarinApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using JuanArangoXamarinApp.ViewModels;
+using JuanArangoXamarinApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace JuanArangoXamarinApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ JuanArangoXamarinApp/JuanArangoXamarinApp/JuanArangoXamarinApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using JuanArangoXamarinApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace JuanArangoXamarinApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
